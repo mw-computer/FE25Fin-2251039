3bd66590f91860f9c8716343ad537b2a545da393
HTW <2251039@mokwon.ac.kr>
2025-06-14 22:10:02 +0900

    1차 수정

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-//TODO: 동적 기능 구현
+const problems = [
+  { title: "두 수의 합", level: "Lv.1" },
+  { title: "K번째수", level: "Lv.1" },
+  { title: "가장 큰 수", level: "Lv.2" },
+  { title: "타겟 넘버", level: "Lv.2" },
+  { title: "네트워크", level: "Lv.3" },
+  { title: "여행경로", level: "Lv.3" },
+];
+
+const problemList = document.getElementById('problemList');
+problems.forEach(problem => {
+  const li = document.createElement('li');
+  li.innerHTML = `
+    <span class="problem-title">${problem.title}</span>
+    <span class="problem-level">${problem.level}</span>
+  `;
+  problemList.appendChild(li);
+});
